865c4320afa7b806b4981304c219109c1d3860fb
Courtney Dyson <courtneydyson87@gmail.com>
2019-07-09 00:57:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,16 @@
 def consolidate_cart(cart)
-  # code here
+  consolidate_hash = {}
+  cart.each do |item|
+    item.each do |item_name, item_hash|
+      if consolidate_hash.key?(item_name) == false
+        consolidate_hash[item_name] = item_hash
+        consolidate_hash[item_name][:count] = 1
+      else
+        consolidate_hash[item_name][:count] += 1
+      end
+    end
+  end
+  consolidate_hash
 end
 
 def apply_coupons(cart, coupons)
